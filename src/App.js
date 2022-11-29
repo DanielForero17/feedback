6d0c98ee2b8160b9624d9cf9363cac732bfe4efa
cristian <cfgarcia654@misena.edu.co>
2022-11-29 10:33:43 -0500

    firt commit

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import { useState } from "react";
 //data
 import Resenias from "./data/Resenias";
 //componentes
+import Header from "./componentes/Header";
 import Resenia from "./componentes/Resenia";
 import ReseniaList from "./componentes/ReseniaList";
 
 function App(){
 
-  //crear estado para arreglo de resenias
-  const [lista_resenias, setListaResenias] = useState(Resenias)
+    //crear estado inicial para arreglo de resenias
+    const [lista_resenias ,
+           setListaResenias  ] = useState(Resenias)
 
-  //metodo para borrar una resenia:
-  const deleteResenia = id => {
-    window.confirm("seguro bro quieres borrar la resenia")
+    //metodo para borrar una resenia:
+    const deleteResenia = id => {
+        window.confirm("estas seguro de borrar la resenia")
+    }       
 
-
-  }
-
-  return (
-    <div className="container">
-      <ReseniaList 
-      deleteResenia={deleteResenia}
-      listaresenias={lista_resenias} />
-
-    </div>
-  )
+    return (
+        <div className="container">
+             <ReseniaList
+                deleteResenia={deleteResenia}
+                listaresenias={lista_resenias}  />
+        </div>
+        
+    )
 }
 export default App;
--- src/componentes/ReseniaList.jsx
+++ src/componentes/ReseniaList.jsx
@@ -1,30 +1,30 @@
-
 import Resenia from "./Resenia"
 
-const ReseniaList = ( { listaresenias, deleteResenia } ) => {
+const ReseniaList = ( { listaresenias, deleteResenia  } ) => {
 
-            //condicional en caso de que no llegue el arreglo//
-            if(!listaresenias || listaresenias.legth === 0){
-                return  <p>No hay reseñas</p>
-            }else{
-                return (
-                <div className="feedbak-list" >
-                { listaresenias.map((resenia)=>
-                (
-                <Resenia 
-                deleteResenia={deleteResenia}
-                key={resenia.id} 
-                resenia={resenia} />
-                )
-                )
-                }
+     //condicional en caso de 
+        // que no llegue el arreglo
+        if(!listaresenias || listaresenias.length === 0 ){
+            return   <p>No hay reseñas</p> 
+        }else{
+            return (
+                <div className="feedback-list">
+                    {/* Recorrer la listaresenias */}
+                    { listaresenias.map((resenia)=>
+                               (
+                                <Resenia
+                                    deleteResenia={ deleteResenia }
+                                    key={resenia.id}
+                                    resenia={resenia}  />
+                               ) 
+                            ) 
+                    } 
                 </div>
-                
             )
-                    
-                    
         }
-                  
-    }             
 
-export default ReseniaList; 
+
+    
+}
+
+export default ReseniaList
